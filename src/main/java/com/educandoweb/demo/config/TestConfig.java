a1969505dcc0fb5cdbdef2171297a238d741d506
ScavazzaRian <rianscavazza.git@gmail.com>
2026-02-13 14:11:53 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/educandoweb/demo/config/TestConfig.java
+++ src/main/java/com/educandoweb/demo/config/TestConfig.java
@@ -60,5 +60,14 @@ public class TestConfig implements CommandLineRunner {
 		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
 
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+		
+		p1.getCategory().add(cat2);
+		p2.getCategory().add(cat1);
+		p2.getCategory().add(cat3);
+		p3.getCategory().add(cat3);
+		p4.getCategory().add(cat3);
+		p5.getCategory().add(cat2);
+		
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 	}
 }
